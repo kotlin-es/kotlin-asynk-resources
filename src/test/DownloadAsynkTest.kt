52b2bb2a769debbcadfd5d3ccd00405294b1e086
vicboma1 <victorbolinchesmarin@gmail.com>
2016-12-28 21:04:30 +0100

    add test

--- src/test/DownloadAsynkTest.kt
+++ src/test/DownloadAsynkTest.kt
@@ -1,4 +1,5 @@
 
+import map.entry.MapEntry
 import org.junit.Assert
 import org.junit.Assert.fail
 import java.util.*
@@ -10,11 +11,21 @@ import java.util.concurrent.CompletableFuture
 class DownloadAsynkTest {
 
     @org.junit.Test
-    fun testSubmit1() {
-        val listCompletable = DownloadAsynk.create().submit(
+    fun testSingleton() {
+        val obj1 = DownloadAsynk
+        val obj2 = DownloadAsynk
+
+        Assert.assertTrue( obj1 == obj2 )
+        Assert.assertTrue( obj1.toString().equals(obj2.toString()))
+
+    }
+
+    @org.junit.Test
+    fun testSubmit() {
+        val listCompletable = DownloadAsynk.submit(
            object : HashMap<String, String>() {
                        init {
-                           put("https://github.com/kotlin-es/kotlin-asynk-resources/blob/master/src/main/resource/AeroStar(J)%5B!%5D.zip", "Aero-Star.zip")
+                           put("https://github.com/kotlin-es/kotlin-asynk-resources/blob/master/src/main/resource/AeroStar(J)%5B!%5D.zip", "./src/Aero-Star.zip")
                            put("https://github.com/kotlin-es/kotlin-asynk-resources/blob/master/src/main/resource/Alien3(J)%5B!%5D.zip", "Alien-3.zip")
                            put("https://github.com/kotlin-es/kotlin-asynk-resources/blob/master/src/main/resource/SuperMarioLand2-6GoldenCoins(UE)(V1.2)%5B!%5D.zip","Super-Mario-Land-2-6-Golden-Coins.zip")
                        }
@@ -34,10 +45,33 @@ class DownloadAsynkTest {
                     fail()
                 }
             }
-
-
-
         }
+
+        System.out.println("End Test")
+
+        runAsync.get()
+    }
+
+
+    @org.junit.Test
+    fun testSubmit2() {
+        val completable = DownloadAsynk.submit(
+                MapEntry("https://github.com/kotlin-es/kotlin-asynk-resources/blob/master/src/main/resource/AeroStar(J)%5B!%5D.zip", "Aero-Star.zip")
+        )
+
+
+        val runAsync = CompletableFuture.runAsync {
+                try {
+                    val file = completable.get()
+                    val res = file.exists()
+                    System.out.println("Assert " + res.toString())
+                    Assert.assertTrue(res)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    fail()
+                }
+        }
+
 
         System.out.println("End Test")
 
